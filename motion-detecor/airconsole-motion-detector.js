986334281a217aaa57acbc5bda71e4bdec9708f5
two <alexward@nevada.unr.edu>
2022-12-10 20:01:39 -0800

    fix: fix tilt controls for ios

--- motion-detecor/airconsole-motion-detector.js
+++ motion-detecor/airconsole-motion-detector.js
@@ -14,24 +14,19 @@
  * @param {Config} opts - Constructor config.
  * @constructor
  */
+var options = {};
+var ball = null;
+var backgroundElement = null
+
 function AirConsoleMotionDetector(opts, elementId) {
-  opts = opts || {};
-  this.deadzone = opts.deadzone || 2;
-  this.sensitivity = opts.sensitivity || 3;
-  this.invertX = opts.invertX || false;
-  this.invertY = opts.invertY || false;
-  this.horizontalOnly = opts.horizontalOnly || false;
-  this.verticalOnly = opts.verticalOnly || false;
+  options = opts || {};
+  ball = null;
 
-  this.tiltMove = opts.tiltMove || function(data) { console.log ("Moving ...", data) };
-
-  const backgroundElement = document.getElementById(elementId)
-
+  backgroundElement = document.getElementById(elementId)
   if(!backgroundElement){
     console.error("ERROR! Motion pad element not found!", elementId);
   }
 
-  var ball = null;
   for (var i = 0; i < backgroundElement.childNodes.length; i++) {
     if (backgroundElement.childNodes[i].className == "motion-pad-ball") {
       ball = backgroundElement.childNodes[i];
@@ -41,44 +36,91 @@ function AirConsoleMotionDetector(opts, elementId) {
 
   if(!ball){
     console.error("ERROR! Motion pad ball not found!", elementId);
+  } else {
+    console.error("Motion pad ball found!", ball);
   }
 
-  window.addEventListener("deviceorientation", () => {
-    var absolute = event.absolute;
-    var alpha    = event.alpha;
-    var beta     = event.beta;
-    var gamma    = event.gamma;
+  setOrientationListener()
+}
 
-    var x = event.beta;  // In degree in the range [-180,180)
-    var y = event.gamma; // In degree in the range [-90,90)
+function setOrientationListener(){
+  if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
+    // (optional) Do something before API request prompt.
+    DeviceMotionEvent.requestPermission()
+        .then( response => {
+          console.log(response)
+          if ( response == "granted" ) {
+            window.addEventListener( "deviceorientation", deviceOrientationFunction, true)
+          }
+        })
+        .catch((error) => {
+          console.error(error)
 
-    const ballPercentX = (ball.clientWidth / backgroundElement.clientWidth * 100) ;
-    const ballPercentY = (ball.clientHeight / backgroundElement.clientHeight * 100);
+          if (error.message.includes("requires a user gesture")) {
+            promptUser()
+          }
+        })
+  } else {
+    window.addEventListener( "deviceorientation", deviceOrientationFunction, true)
+  }
+}
 
-    const eventData = {};
-    y = normalizeInput(y, ballPercentY, this.deadzone, this.sensitivity, this.invertY);
-    x = normalizeInput(x, ballPercentX, this.deadzone, this.sensitivity, this.invertX);
+function promptUser(){
+  const modalText = document.getElementById('permission-modal-text')
+  if (modalText) {
+    modalText.innerText = "Grant motion detection permission for tilt controlls?"
+    $('#permission_modal_button_click').click( function() {
+      setOrientationListener()
+    });
 
-    eventData['x'] = ((x / (100 - ballPercentX)) - .5) * 2;
-    eventData['y'] = ((y / (100 - ballPercentY)) - .5) * 2;
+    const popupModal = $('#popupModalPermission')
+    popupModal.modal('show')
+  }
+}
+function deviceOrientationFunction() {
+  // init params
+  let deadzone = options.deadzone || 2;
+  let sensitivity = options.sensitivity || 3;
+  let invertX = options.invertX || false;
+  let invertY = options.invertY || false;
+  let horizontalOnly = options.horizontalOnly || false;
+  let verticalOnly = options.verticalOnly || false;
+  let tiltMove = options.tiltMove || function(data) { console.log ("Moving ...", data) };
 
-    ball.style.top = y + '%';
-    ball.style.left = x + '%';
+  // run math
+  var absolute = event.absolute;
+  var alpha    = event.alpha;
+  var beta     = event.beta;
+  var gamma    = event.gamma;
 
-    if(this.verticalOnly){
-      ball.style.left = (50 - (ballPercentX/2)) + '%'
-      delete eventData['x'];
-    }
+  var x = event.beta;  // In degree in the range [-180,180)
+  var y = event.gamma; // In degree in the range [-90,90)
 
-    if(this.horizontalOnly){
-      ball.style.top = (50 - (ballPercentY/2)) + '%'
-      delete eventData['y'];
-    }
+  const ballPercentX = (ball.clientWidth / backgroundElement.clientWidth * 100) ;
+  const ballPercentY = (ball.clientHeight / backgroundElement.clientHeight * 100);
 
-    // Do stuff with the new orientation data
-    this.tiltMove(eventData)
+  const eventData = {};
+  y = normalizeInput(y, ballPercentY, deadzone, sensitivity, invertY);
+  x = normalizeInput(x, ballPercentX, deadzone, sensitivity, invertX);
 
-  }, true);
+  eventData['x'] = ((x / (100 - ballPercentX)) - .5) * 2;
+  eventData['y'] = ((y / (100 - ballPercentY)) - .5) * 2;
+
+  ball.style.top = y + '%';
+  ball.style.left = x + '%';
+
+  if(verticalOnly){
+    ball.style.left = (50 - (ballPercentX/2)) + '%'
+    delete eventData['x'];
+  }
+
+  if(horizontalOnly){
+    ball.style.top = (50 - (ballPercentY/2)) + '%'
+    delete eventData['y'];
+  }
+
+  // Do stuff with the new orientation data
+  tiltMove(eventData)
 }
 
 function normalizeInput(input, ballPercent, deadzone, sensitivity, invert){
@@ -104,6 +146,5 @@ function normalizeInput(input, ballPercent, deadzone, sensitivity, invert){
   } else if (input > (100 - ballPercent)){
     input = 100 - ballPercent;
   }
-
   return input;
 }
